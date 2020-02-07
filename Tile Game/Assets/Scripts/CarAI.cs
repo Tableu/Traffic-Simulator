6fe9953883ddae830f8370a7afb70e78d85e2825
darthvader99999 <bhow245@gmail.com>
2020-02-06 23:56:24 -0800

    init

---
+++ Tile Game/Assets/Scripts/CarAI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector2 velocity;
+    public Rigidbody2D r;
+    private Spawner parent;
+    void Start()
+    {
+        parent = transform.parent.GetComponent<Spawner>();
+        velocity = new Vector2(1, 0);
+    }
+    void OnBecameInvisible(){
+        Destroy(gameObject);
+    }
+    void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag == "Car"){
+            collision.gameObject.GetComponent<CarAI>().velocity = new Vector2(0,0);
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision){
+        if(collision.gameObject.tag == "Car"){
+            StartCoroutine(start());
+        }
+    }
+    IEnumerator start(){
+            //Debug.Log("Started Coroutine at timestamp : " + Time.time);
+            yield return new WaitForSeconds(1);
+            velocity = new Vector2(1,0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        r.MovePosition(r.position + velocity * Time.fixedDeltaTime);
+    }
+}
---
+++ Tile Game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public Object[] sprites;
+    public float timer;
+    private Vector2 size;
+    private BoxCollider2D collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //prefabArray = new GameObject[3];
+        timer = 0;
+        sprites = Resources.LoadAll("Vehicles", typeof(Sprite));
+        collider = gameObject.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(collider.IsTouchingLayers(Physics2D.AllLayers) == false){
+            timer -= 1;
+        }
+        if(timer <= 0){
+            timer = Random.Range(100,200);
+            GameObject car = Instantiate(prefab, gameObject.transform.position, Quaternion.identity);
+            car.transform.parent = this.transform; 
+            car.GetComponent<SpriteRenderer>().sprite = (Sprite)sprites[Random.Range(0,sprites.Length)];
+            car.AddComponent<BoxCollider2D>();
+            size = car.GetComponent<BoxCollider2D>().size;
+            car.GetComponent<BoxCollider2D>().size = new Vector2(size.x + (float)0.12, size.y);
+        }
+    }
+}
